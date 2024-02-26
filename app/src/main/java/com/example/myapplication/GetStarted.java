24ced25bac7ea16fe75af9e16118f7231a9f9b8a
Rafi <rafikhoriul65@gmail.com>
2024-02-26 14:15:35 +0700

    Commit

--- app/src/main/java/com/example/myapplication/GetStarted.java
+++ app/src/main/java/com/example/myapplication/GetStarted.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GetStarted extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class GetStarted extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
+        Button button = findViewById(R.id.gtsstarted);
+
+    }
+    public void lopelope(View view){
+        Intent i = new Intent(this, MainActivity2.class);
+        startActivity(i);
     }
 }
